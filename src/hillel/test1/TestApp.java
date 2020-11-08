42ce5e659785ea727c81b27ed402b5c9e5e92d5e
Andrii Tykhonov <tianodessa@gmail.com>
2020-11-08 19:13:01 +0200

    One more commit

--- src/hillel/test1/TestApp.java
+++ src/hillel/test1/TestApp.java
@@ -3,5 +3,6 @@ package hillel.test1;
 public class TestApp {
     public static void main(String[] args) {
         System.out.println("Test application!!");
+        System.out.println("Test out");
     }
 }
